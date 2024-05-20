41e5c413539913a15adf8d3b4c052d03ef96e9cb
Calcent <134114315+Calcent@users.noreply.github.com>
2024-05-20 12:02:49 -0700

    Changes for GUI
    Added login fxml along with the controller class for MainGUI and LoginGUI

---
+++ group project v2/StatusPlusPlus/src/main/java/com/example/statusplusplus/Classes/LoginGUI.java
@@ -0,0 +1,18 @@
+package com.example.statusplusplus.Classes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginGUI {
+
+    @FXML
+    private TextField passwordBox;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private TextField usernameBox;
+
+}
---
+++ group project v2/StatusPlusPlus/src/main/java/com/example/statusplusplus/Classes/mainGUI.java
@@ -0,0 +1,73 @@
+package com.example.statusplusplus.Classes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+public class mainGUI {
+
+    @FXML
+    private Text exp;
+
+    @FXML
+    private Text intel;
+
+    @FXML
+    private Text level;
+
+    @FXML
+    private TextArea notes;
+
+    @FXML
+    private Button shopOpenButton;
+
+    @FXML
+    private Text sta;
+
+    @FXML
+    private Text str;
+
+    @FXML
+    private Text streak;
+
+    @FXML
+    private Text task1;
+
+    @FXML
+    private Button task1button;
+
+    @FXML
+    private Text task2;
+
+    @FXML
+    private Button task2button;
+
+    @FXML
+    private Text task3;
+
+    @FXML
+    private Button task3button;
+
+    @FXML
+    private Text task4;
+
+    @FXML
+    private Button task4button;
+
+    @FXML
+    private Text task5;
+
+    @FXML
+    private Button task5button;
+
+    @FXML
+    private Text username;
+
+    @FXML
+    private Text vit;
+
+    @FXML
+    private Text wis;
+
+}

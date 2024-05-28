e3422f206c5383f6e990166e7913fe56f04e193e
Wyatt <wyattwooden5@gmail.com>
2024-05-28 15:45:38 -0700

    Add driver class for the UserAddsTasks class

---
+++ group project v2/StatusPlusPlus/src/main/java/com/example/statusplusplus/TestDrivers/UserAddsTasksDriver.java
@@ -0,0 +1,37 @@
+/**
+ * Driver class for the "Add Tasks" page
+ * User is able to filter through categories and select tasks
+ */
+package com.example.statusplusplus.TestDrivers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UserAddsTasksDriver extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Load the FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/statusplusplus/Status++UserAddsTasks.fxml"));
+
+        // Set the scene
+        Scene scene = new Scene(root);
+
+        // Set the stage title
+        primaryStage.setTitle("Add Tasks");
+
+        // Set the scene on the primary stage
+        primaryStage.setScene(scene);
+
+        // Show the primary stage
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
